3fcbc27ba0d2d530ea04a88028fef4ed15186990
Muhammad Afdiansyah <91107370+muhafdiansyah@users.noreply.github.com>
2022-09-10 13:41:17 +0700

    + Add User, Login and Security page enhanchement

--- frontend/capstone-webapp/src/App.js
+++ frontend/capstone-webapp/src/App.js
@@ -1,11 +1,13 @@
 import Authentication from './pages/Authentication.js';
 import LoginForm from './pages/LoginForm.js'
 import AccManagement from './pages/AccManagement'
+import AddUser from './components/AddUser.js'
+import './App.css';
 
 function App() {
   return (
     <div className="page">
-      <AccManagement/>
+      <AddUser/>
     </div>
   );
 }
