53a6450541f36a92e66c7366085a3222821ccf60
Muhammad Afdiansyah <91107370+muhafdiansyah@users.noreply.github.com>
2022-09-10 15:39:50 +0700

    Add User Modal

--- frontend/capstone-webapp/src/App.js
+++ frontend/capstone-webapp/src/App.js
@@ -1,13 +1,12 @@
 import Authentication from './pages/Authentication.js';
 import LoginForm from './pages/LoginForm.js'
 import AccManagement from './pages/AccManagement'
-import AddUser from './components/AddUser.js'
 import './App.css';
 
 function App() {
   return (
     <div className="page">
-      <AddUser/>
+      <AccManagement/>
     </div>
   );
 }
--- frontend/capstone-webapp/src/pages/AccManagement.js
+++ frontend/capstone-webapp/src/pages/AccManagement.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/AccManagement.css';
-import { Button, Typography, Grid, createTheme,ThemeProvider } from '@mui/material';
+import { Card, CardContent,TextField, Button, Typography, Grid, createTheme,ThemeProvider } from '@mui/material';
 
 const AccManagement = () =>  {
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
 
   const theme = createTheme({
     typography: {
@@ -16,14 +22,14 @@ const AccManagement = () =>  {
         <div class="main">
 
           <div class="add2-btn">
-          <ThemeProvider theme={theme} >
-            <Grid>
-                <Grid item xs={12}>
-                    <Button type="submit" /*onClick={}*/ variant="outlined" color="primary" value="" style={{ backgroundColor: '#5572c7', color: '#FFFFFF', textTransform: 'none'}}>
-                        <Typography style={{ fontWeight: 500 }}>Add User</Typography></Button>
-                </Grid>
-            </Grid>
-          </ThemeProvider>
+            <ThemeProvider theme={theme} >
+              <Grid>
+                  <Grid item xs={12}>
+                      <Button type="submit" onClick={toggleModal} variant="outlined" color="primary" value="" style={{ backgroundColor: '#5572c7', color: '#FFFFFF', textTransform: 'none'}}>
+                          <Typography style={{ fontWeight: 500 }}>Add User</Typography></Button>
+                  </Grid>
+              </Grid>
+            </ThemeProvider>
           </div>
 
           <table class="content-table">
@@ -58,11 +64,81 @@ const AccManagement = () =>  {
                     </td>
                   </tr>
                 </tbody>
-          </table>
+          </table>     
 
-                
-           
+        {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content">
+              <div className="cover-add">
+                <ThemeProvider theme={theme} >
+                    <Grid>
+                        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+                        
+                        <CardContent>
+                        
+                        <Grid>
+                            <div>
+                                <button className="close-modal close-btn" onClick={toggleModal}>
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </Grid>
+                        
+
+                        <Grid container spacing={1}>
+                            <div class="header">
+                                <Typography gutterBottom variant="h5" align="center" style={{ fontWeight: 600 }} color="#333333">
+                                    User Setting
+                                </Typography> 
+                            </div>
+                        </Grid>
+                        
+
+                            <form /*onSubmit={}*/>
+                            <Grid container spacing={1}>
+
+                                <Grid item xs={12}>
+                                <TextField id="" name="Username" placeholder="Enter Username" label="Username" variant="outlined" 
+                                /*onChange= {}*/ fullWidth required />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                <TextField id="" name="Role" placeholder="Choose Role" label="Role" type="password" variant="outlined"  
+                                /*onChange= {}*/ fullWidth required select />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                <TextField id="" name="Authority" placeholder="Choose Authority" label="Authority" variant="outlined"  
+                                /*onChange= {}*/ fullWidth required select />
+                                </Grid>
+
+                                
+                            </Grid>
+
+                            
+                            <div class="login-btn">
+                                <Grid>
+                                    <Grid item xs={12}>
+                                        <Button type="submit" /*onClick={}*/ variant="outlined" color="primary" value="" style={{ backgroundColor: '#5572c7', color: '#FFFFFF', textTransform: 'none'}} fullWidth>
+                                            <Typography style={{ fontWeight: 500 }}>Save</Typography></Button>
+                                    </Grid>
+                                </Grid>
+                            </div>
+
+                            </form>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+                </ThemeProvider>
+                </div>
+              </div>
+            </div>
         </div>
+        )}
+        </div>
+
+        
     )
 }
 
