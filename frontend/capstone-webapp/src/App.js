afe73c20a825be9fac85f2fb137ddb4e2532a403
Muhammad Afdiansyah <91107370+muhafdiansyah@users.noreply.github.com>
2022-09-13 21:23:57 +0700

    Add Authority Preset Table

--- frontend/capstone-webapp/src/App.js
+++ frontend/capstone-webapp/src/App.js
@@ -1,13 +1,14 @@
 import Authentication from './pages/Authentication.js';
 import LoginForm from './pages/LoginForm.js'
 import AccManagement from './pages/AccManagement'
+import Authority from './pages/Authority'
 import AddUser from './components/AddUser'
 import './App.css';
 
 function App() {
   return (
     <div className="page">
-      <AccManagement/>
+      <Authority/>
     </div>
   );
 }
---
+++ frontend/capstone-webapp/src/pages/Authority.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import '../styles/Authority.css';
+import AddUser from '../components/AddUser.js';
+import { Button, Typography, Grid, createTheme,ThemeProvider } from '@mui/material';
+
+const AccManagement = () =>  {
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
+
+  const theme = createTheme({
+
+    palette: {
+      primary: {
+        main: "#5572c7"},
+      secondary: {
+        main: "#db4d4d"
+      }
+    },
+    typography: {
+      fontFamily: [
+        'Poppins',
+        'sans-serif',
+      ].join(','),
+    },});
+
+    return (
+        <div class="main">
+
+          <div class="add2-btn">
+            <ThemeProvider theme={theme} >
+              <Grid>
+                  <Grid item xs={12}>
+                      <Button type="submit" onClick={toggleModal} variant="contained" color="primary" value="" style={{ textTransform: 'none'}}>
+                          <Typography style={{ fontWeight: 500 }}>Add User</Typography></Button>
+                  </Grid>
+              </Grid>
+            </ThemeProvider>
+          </div>
+
+          <table class="content-table">
+                <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Authorities</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>A</td>
+                    <td>Authority1, Authority2</td>
+                    <td>
+                      <button type="button" class="act-btn edit-btn"><i class="fas fa-edit"></i></button>
+                      <button type="button" class="act-btn del-btn"><i class="fa fa-trash"></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>B</td>
+                    <td>Authority1, Authority2</td>
+                    <td>
+                      <button type="button" class="act-btn edit-btn"><i class="fas fa-edit"></i></button>
+                      <button type="button" class="act-btn del-btn"><i class="fa fa-trash"></i></button>
+                    </td>
+                  </tr>
+                </tbody>
+          </table>     
+
+        {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content">
+               <AddUser/>
+               <div>
+                <button className="btn-cancel" onClick={toggleModal} style={{ textTransform: 'none'}}><p>Cancel</p></button>
+               </div>
+            </div>
+          </div>
+        </div>
+        )}
+        </div>
+
+    )
+
+
+}
+
+export default AccManagement
