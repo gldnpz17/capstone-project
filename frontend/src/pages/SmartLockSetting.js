3cff407c8455430f89423e02655579a5006d72b1
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-11-08 09:44:07 +0700

    Fixed some modal errors.

--- frontend/src/pages/SmartLockSetting.js
+++ frontend/src/pages/SmartLockSetting.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import "../styles/SmartLockSetting.css";
-import { Button, CardContent, Typography, Card, Grid, TextField, Tabs, Tab, Box, MenuItem } from '@mui/material';
+import { Button, CardContent, Typography, Card, Grid, TextField, Tabs, Tab, Box, MenuItem, Stack, IconButton } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import theme from '../components/UItheme';
 import { useMutation, useQuery } from "@apollo/client";
@@ -8,29 +8,7 @@ import { CREATE_AUTHORIZATION_RULE, READ_ALL_AUTHORIZATION_RULES } from "../quer
 import { TabPanel } from "../components/TabPanel";
 import { AuthorizationRuleArgsForm } from "../components/AuthorizationRuleArgsForm";
 import { READ_ALL_LOCKS, UPDATE_SMART_LOCK_RULE } from "../queries/SmartLocks";
-
-const GeneralSection = ({}) => {
-    return (
-        <Grid container spacing={1} sx={{ py: 2 }}>
-            <Grid item xs={12}>
-                <TextField id="" name="Name" placeholder="Enter Name" label="Name" variant="outlined" 
-                /*onChange= {}*/ fullWidth required />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField id="" name="wifiSsid" placeholder="My WiFi" label="WiFi SSID" variant="outlined" 
-                /*onChange= {}*/ fullWidth required />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField id="" name="wifiPassword" label="WiFi Password" variant="outlined" 
-                /*onChange= {}*/ fullWidth required />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField id="" name="Connected-Device" label="Connected Device" variant="outlined"  
-                /*onChange= {}*/ fullWidth required />
-            </Grid>
-        </Grid>
-    )
-}
+import { Edit } from "@mui/icons-material";
 
 const SecuritySection = ({ lock, authorizationRules }) => {
     const [createAuthorizationRule] = useMutation(CREATE_AUTHORIZATION_RULE)
@@ -139,7 +117,6 @@ const SecuritySection = ({ lock, authorizationRules }) => {
 }
 
 function SmartLockSetting({ lock }) {
-    const [tab, setTab] = useState(0)
     const {
         data: { authorizationRules } = {}
     } = useQuery(READ_ALL_AUTHORIZATION_RULES)
@@ -147,32 +124,26 @@ function SmartLockSetting({ lock }) {
     return(
         <div className="cover-setting" style={{ zIndex: 3000 }}>
             <ThemeProvider theme={theme} >                    
-                <div className="cover-head">
-                    <Typography className="header" gutterBottom variant="h5" align="left" style={{ fontWeight: 800 }} color="#333333">
+                <Stack sx={{ p: 2 }} gap={1}>
+                    <Typography variant="h5" align="left" style={{ fontWeight: 800 }} color="#333333">
                         Settings
                     </Typography> 
-
-                    <Typography className="header" gutterBottom align="left" style={{ fontWeight: 500 }} color="#333333">
-                        Lock: E6
-                    </Typography>    
-                </div>
+                    <Stack direction="row" alignItems="center" gap={1}>
+                        <Typography align="left" style={{ fontWeight: 500 }} color="#333333">
+                            <b>Lock :</b> E6
+                        </Typography>
+                        <IconButton>
+                            <Edit />
+                        </IconButton>
+                    </Stack>
+                    <Typography>
+                        <b>Device MAC Address :</b> N/A
+                    </Typography>
+                </Stack>
                 <Grid>
-                    <Card style={{ width: 500, padding: "2px 5px", margin: "0 auto" }}>
+                    <Card style={{ width: 500, p: 2, margin: "0 auto" }}>
                         <CardContent>
-                            <Box sx={{ width: '100%' }}>
-                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                    <Tabs value={tab} onChange={(_, tab) => setTab(tab)}>
-                                        <Tab label="Configuration" />
-                                        <Tab label="Security" />
-                                    </Tabs>
-                                </Box>
-                                <TabPanel value={tab} index={0}>
-                                    <GeneralSection />
-                                </TabPanel>
-                                <TabPanel value={tab} index={1}>
-                                    <SecuritySection {...{ authorizationRules, lock }} />
-                                </TabPanel>
-                            </Box>
+                            <SecuritySection {...{ authorizationRules, lock }} />
                         </CardContent>
                     </Card>
                 </Grid>
